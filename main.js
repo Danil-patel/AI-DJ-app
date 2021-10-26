1dbf98915f2a542deaeec7e9497749399892b074
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-10-26 17:39:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -6,10 +10,12 @@ function preload() {
 
 function setup() {
     canvas = createCanvas(600,500);
-    canvas.center();
+    canvas.position(650,300);
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw() {
@@ -18,4 +24,22 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('leftWristX is ' + leftWristX + ' leftWristY is ' + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('rightWristX is ' + rightWristX + ' rightWristY is ' + rightWristY);
+    }
 }
