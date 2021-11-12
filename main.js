b7d71632c47c61bcb452fd24e293b1e82c589828
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-11-12 17:34:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,31 +3,58 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function preload() {
     song = loadSound("music.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(600,500);
-    canvas.position(650,300);
+    canvas = createCanvas(600, 500);
+    canvas.position(650, 300);
 
     video = createCapture(VIDEO);
     video.hide();
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-    image(video,0,0,600,500);
+    image(video, 0, 0, 600, 500);
     fill('#FF0000');
     stroke('#FF0000');
-    circle(leftWristX,leftWristY,20);
-    inNumberLeftWristY = Number(leftWristY);
-    remove_decimals = floor(inNumberLeftWristY);
-    volume = remove_decimals/500;
-    document.getElementById("volume").innerHTML = "volume = " + volume;
-    song.setVolume(volume);
+    if(scoreRightWrist>0.2) {
+    circle(leftWristX, leftWristY, 20);
+    if (rightWristY > 0 && rightWristY <= 100) {
+        document.getElementById("speed").innerHTML = "speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if (rightWristY > 100 && rightWristY <= 200) {
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    }
+    else if (rightWristY > 200 && rightWristY <= 300) {
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if (rightWristY > 300 && rightWristY <= 400) {
+        document.getElementById("speed").innerHTML = "speed = 2x";
+        song.rate(2);
+    }
+    else if (rightWristY > 400 && rightWristY <= 500) {
+        document.getElementById("speed").innerHTML = "speed = 2.5x";
+        song.rate(2.5);
+    }
+}
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        inNumberLeftWristY = Number(leftWristY);
+        remove_decimals = floor(inNumberLeftWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("volume").innerHTML = "volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play() {
@@ -41,8 +68,11 @@ function modelLoaded() {
 }
 
 function gotPoses(results) {
-    if(results.length>0) {
+    if (results.length > 0) {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log('scoreRightWrist = ' + scoreRightWrist + ' scoreleftWrist = ' + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log('leftWristX is ' + leftWristX + ' leftWristY is ' + leftWristY);
